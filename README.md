271056e7846ec71c645ce400cdf1f179b1201afb
Rubén Fernández-Marcote de Gracia <ruben.fernandezmarcote@produban.com>
2017-02-24 11:52:47 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MADescaperoomESP
-Arduino program for trigger GPIO ports of ESP8266 
+Arduino program for trigger GPIO ports of ESP8266 from NODE-RED with MQTT
+
+This is an arduino program to be loaded in a ESP8266 that triggers GPIO ports. The trigger is made by NODE-RED inyecting payload in MQTT broker.
+
+This is a WIP project that has started with the examples of OTA and WifiManager
+
+## Main Features
+### OTA
+As this small chips can be in places with a limited access it's important the ability to reprogram them without physical contact. 
+For this feature ESP8266 with 4MB it's needed. I'm using wemos D1 r2
+
+### Captative Portal
+We are going to have a bunch of ESPs so it's important an easy way of configuration without any IDE (after first programmin) so I dediced to use WifiManager to create a captative portal and configure the Wifi Credentials aswell the MQTT server and the topic it will be listen. 
+
+### MQTT subscriber
+ESP8266 will be listenting a topic and will be notified about when trigger a certain GPIO
---
+++ wemos_D1_8266.ino
@@ -0,0 +1,268 @@
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <WiFiManager.h>   //https://github.com/tzapu/WiFiManager
+#include <DNSServer.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include <PubSubClient.h>
+
+int pinTrigger = 1;                 // Trigger connected to pin1
+
+// wifi manager parameters
+//define your default values here, if there are different values in config.json, they are overwritten.
+//char mqtt_server[40] = "" ;
+//char mqtt_port[6] = "";
+//char mqtt_topic[34] = "";
+
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+WiFiManager wifiManager;
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+
+//callback for  MQTT message received
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    Serial.print("Encendido");
+    // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+
+    pinMode(pinTrigger, OUTPUT); 
+    digitalWrite(pinTrigger, LOW);
+    
+  } else {
+    pinMode(pinTrigger, INPUT); 
+    Serial.print("apagado");
+   }
+}
+
+
+//---SETUP------------------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting...");
+ 
+  //clean FS, for testing
+  //SPIFFS.format();
+
+
+ //read configuration from FS json
+  Serial.println("mounting FS...");
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_topic, json["mqtt_topic"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
+  WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 32);
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_topic);
+  
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.autoConnect("AutoConnectAP");
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  Serial.println("connected...yeey :)");
+  
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_topic, custom_mqtt_topic.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_topic"] = mqtt_topic;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+
+
+
+String clientName;
+clientName = "paint";
+
+    if (client.connect((char*) clientName.c_str())) {
+    Serial.println("Connected to MQTT broker");
+    Serial.print("Topic is: ");
+    Serial.println(mqtt_topic);
+    
+  }
+  else {
+    Serial.println("MQTT connect failed");
+    Serial.println("Will reset and try again...");
+    abort();
+  }
+  }
+
+     Serial.println("servidor mqtt:");
+     Serial.println(mqtt_server);
+     
+    client.setServer(mqtt_server, atoi(mqtt_port));
+    Serial.println("configuring callback");
+    client.setCallback(callback); 
+
+
+
+
+// OTA
+    
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  //ArduinoOTA.setHostname("ESP-clock");
+
+  // No authentication by default
+  //ArduinoOTA.setPassword((const char *)"1234");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+//END OTA
+
+}
+//------------END SETUP -----------------------------
+
+
+
+
+
+//reconect MQTT
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // TODO: notify new client is connected 
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  
+   if (!client.connected()) {
+    reconnect();
+   }
+   
+   client.loop();
+}
